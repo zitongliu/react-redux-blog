1aeb4887de316ec5ed0a7a885b259312979d7453
zitongliu <zitongliu7@gmail.com>
2017-07-15 23:21:25 +1000

    connected posts index page to redux

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
 
-export default () => {
-  return <div>List of blog posts.</div>
-};
+class PostsIndex extends Component {
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>List of blog posts</div>
+    );
+  }
+}
+
+// we can refactor mapDispatchToProps inside connect
+// function mapDispatchToProps(dispatch) {
+//   return bindActionCreators({ fetchPosts }, dispatch);
+// }
+
+export default connect(null, { fetchPosts })(PostsIndex);
