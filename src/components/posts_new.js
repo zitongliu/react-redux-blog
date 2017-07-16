9c83ed6e7e4ef75e4aced80ea86b75c0d781e20f
zitongliu <zitongliu7@gmail.com>
2017-07-16 17:18:55 +1000

    new post validation wip

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -12,33 +12,64 @@ class PostsNew extends Component {
           type="text"
           {...field.input}
         />
+        {field.meta.error}
       </div>
     )
   }
 
+  onSubmit(values) {
+    console.log(values);
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           label="Title"
           name="title"
           component={this.renderField}
         />
-      <Field
-        label="Tags"
-        name="tags"
-        component={this.renderField}
-      />
-      <Field
-        label="Post Content"
-        name="content"
-        component={this.renderField}
-      />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+      <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     );
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  // Validate the inpurs from 'values'
+  if (!values.title) {
+    errors.title = "Enter a title!";
+  }
+
+  if (!values.categories) {
+    errors.categories = "Enter some categories";
+  }
+
+  if (!values.content) {
+    errors.content = "Enter some content"
+  }
+
+  // If erros is empty, the form is fine to submit
+  // If errors has *any* properties, redux form assumes form is invalid
+  return errors;
+}
+
 export default reduxForm({
+  // validate: validate,
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
