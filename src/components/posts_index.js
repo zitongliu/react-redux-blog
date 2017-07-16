467faf2428e0874847734e465708b83d96fd2f58
zitongliu <zitongliu7@gmail.com>
2017-07-16 13:27:54 +1000

    added react router link and removed routes.js

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,7 +1,9 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 // import { bindActionCreators } from 'redux';
+
 import { fetchPosts } from '../actions/index';
 
 class PostsIndex extends Component {
@@ -22,6 +24,11 @@ class PostsIndex extends Component {
   render() {
     return (
       <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a Post
+          </Link>
+        </div>
         <h3>Posts</h3>
         <ul className="list-group">
           {this.renderPosts()}
