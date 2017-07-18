47521e847a93212b36dcfac1bf2c3bea88b2bd6a
zitongliu <zitongliu7@gmail.com>
2017-07-18 22:17:39 +1000

    created posts show component and route

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import promise from 'redux-promise';
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -17,6 +18,8 @@ ReactDOM.render(
       <div>
         <Switch>
           <Route path="/posts/new" component={PostsNew}/>
+          // react router will take :id and pass it as a prop to PostsShow component
+          <Route path="/posts/:id" component={PostsShow}/>
           <Route path="/" component={PostsIndex} />
         </Switch>
       </div>
